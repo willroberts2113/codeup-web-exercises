5fa71c05db7a2de8c9d9b2e6895082946b2670b6
Will Roberts <willroberts2113@gmail.com>
2022-02-04 13:32:21 -0500

    update

---
+++ js/function_examples_1.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+function isLowerCase(letter){
+    if (letter == letter.toLowerCase()){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function double(n){
+    return n * 2;
+}
+
+function areIdentical(input1, input2){
+    if (input1 === input2) {
+        return true;
+    }else{
+        return false;
+    }
+}
