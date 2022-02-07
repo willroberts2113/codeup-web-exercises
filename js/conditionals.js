659aad276df4f3945a4673407f7e1096762a9454
Will Roberts <willroberts2113@gmail.com>
2022-02-07 17:52:37 -0500

    conditional exercise 1

---
+++ js/conditionals.js
@@ -0,0 +1,189 @@
+"use strict";
+
+/* ########################################################################## */
+
+/**
+ * TODO:
+ * Create a function named `analyzeColor` that accepts a string that is a color
+ * name as input. This function should return a message which relates to the
+ * color stated in the argument of the function. For colors you do not have
+ * responses written for, return a string stating so
+ *
+ * Example:
+ *  > analyzeColor('blue') // returns "blue is the color of the sky"
+ *  > analyzeColor('red') // returns "Strawberries are red"
+ *
+ *
+ *  > analyzeColor('cyan') // returns "I don't know anything about cyan"
+ *
+ * You should use an if-else-if-else block to return different messages.
+ *
+ * Test your function by passing various string literals to it and
+ * console.logging the function's return value
+ */
+/* function analyzeColor(color){
+    if (color == "red"){
+        console.log(color + " is the color of strawberries")
+    }else if(color == "orange"){
+        console.log(color + " is the color of the oranges")
+    }else if(color == "yellow"){
+        console.log(color + " is the color of bananas")
+    }else if(color == "green"){
+        console.log(color + " is the color of emeralds")
+    }else if(color == "blue"){
+        console.log(color + " is the color of the sky")
+    }else if (color == "indigo"){
+        console.log(color + " is the color of sapphires")
+    }else if (color == "violet") {
+        console.log(color + " is the color of amethyst")
+    }else{
+        console.log("I don't know anything about that color")
+    }
+}
+*/
+
+// Don't change the next two lines!
+// These lines create two variables for you:
+// - `colors`: a list of the colors of the rainbow
+// - `randomColor`: contains a single random color value from the list (this
+//                  will contain a different color every time the page loads)
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var randomColor = colors[Math.floor(Math.random() * colors.length)];
+/**
+ * TODO:
+ * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
+ * You should see a different message every time you refresh the page
+ */
+
+// analyzeColor(randomColor)
+
+/**
+ * TODO:
+ * Comment out the code above, and refactor your function to use a switch-case statement
+ */
+
+function analyzeColor(randomColor){
+    switch (randomColor){
+        case("red"):
+            console.log(randomColor + " is the color of strawberries");
+            break;
+        case("orange"):
+            console.log(randomColor + " is the color of the oranges");
+            break;
+        case("yellow"):
+            console.log(randomColor + " is the color of bananas");
+            break;
+        case("green"):
+            console.log(randomColor + " is the color of emeralds");
+            break;
+        case("blue"):
+            console.log(randomColor + " is the color of the sky");
+            break;
+        case("indigo"):
+            console.log(randomColor + " is the color of sapphires");
+            break;
+        case("violet"):
+            console.log(randomColor + " is the color of amethyst");
+            break;
+        default:
+            console.log("I don't know anything about that color");
+            break;
+    }
+}
+
+analyzeColor(requestColor())
+
+/**
+ * TODO:
+ * Prompt the user for a color when the page loads, and pass the input from the
+ * user to your `analyzeColor` function. Alert the return value from your
+ * function to show it to the user.
+ */
+
+function requestColor(){
+    return prompt("What is your favorite color?");
+}
+
+/* ########################################################################## */
+
+/**
+ * TODO:
+ * Suppose there's a promotion in Walmart, each customer is given a randomly
+ * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
+ * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
+ * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
+ * everything for free!.
+ *
+ * Write a function named `calculateTotal` which accepts a lucky number and total
+ * amount, and returns the discounted price.
+ *
+ * Example:
+ * calculateTotal(0, 100) // returns 100
+ * calculateTotal(4, 100) // returns 50
+ * calculateTotal(5, 100) // returns 0
+ *
+ * Test your function by passing it various values and checking for the expected
+ * return value.
+ */
+
+let luckyNumber = Math.floor(Math.random() * 6);
+let totalAmount = 100
+/*console.log(luckyNumber)
+console.log(totalAmount - (totalAmount * .1))
+console.log(totalAmount - (totalAmount * .25))
+console.log(totalAmount - (totalAmount * .35))
+ */
+
+function calculateTotal(luckyNumber, totalAmount) {
+    switch (luckyNumber){
+        case(1):
+            console.log("You receive a 10% discount!  Your total is: " + totalAmount - (totalAmount * .1));
+            break;
+        case(2):
+            console.log("You receive a 25% discount!  Your total is: " + totalAmount - (totalAmount * .25));
+            break;
+        case(3):
+            console.log("You receive a 35% discount!  Your total is: " + totalAmount - (totalAmount * .35));
+            break;
+        case(4):
+            console.log("You receive a 50% discount!  Your total is: " + totalAmount - (totalAmount * .5));
+            break;
+        case(5):
+            console.log("You receive a 100% discount!  Everything is Free!");
+            break;
+        default:
+            console.log("Unfortunately, you do not qualify for a discount.  Your total is: " + totalAmount)
+    }
+}
+
+calculateTotal(luckyNumber, totalAmount)
+
+/**
+ * TODO:
+ * Uncomment the line below to generate a random number between 0 and 5.
+ * (In this line of code, 0 is inclusive, and 6 is exclusive)
+ * Prompt the user for their total bill, then use your `calculateTotal` function
+ * and alerts to display to the user what their lucky number was, what their
+ * price before the discount was, and what their price after the discount is.
+ */
+// Generate a random number between 0 and 6
+// var luckyNumber = Math.floor(Math.random() * 6);
+
+/**
+ * TODO:
+ * Write some JavaScript that uses a `confirm` dialog to ask the user if they
+ * would like to enter a number. If they click 'Ok', prompt the user for a
+ * number, then use 3 separate alerts to tell the user:
+ *
+ * - whether the number is even or odd
+ * - what the number plus 100 is
+ * - if the number is negative or positive
+ *
+ * Do *NOT* display any of the above information
+ * if the user enters a value that is not of the number data type.
+ * Instead, use an alert to inform them of the incorrect input data type.
+ *
+ *
+ * Can you refactor your code to use functions?
+ * HINT: The way we prompt for a value could be improved
+ */
--- js/functions.js
+++ js/functions.js
@@ -113,5 +113,50 @@ function applyDiscount(originalPrice, discountPercent){
     let discount = originalPrice * discountPercent;
     return discount;
 }
-
 applyDiscount(originalPrice, discountPercent);
+
+/* Create a function called getToDestination which will help determine how a person can get to their destination!
+    It will accept 4 arguments and the parameter names are:
+    age - the age of the rider
+isInsured - a boolean telling the function if the rider has insurance
+hasCar - a boolean describing if the rider has a car
+canGetRideshare - boolean describing if the rider can get a ride share (Uber, etc)
+If the rider is over 16, has insurance, and has a car then they can use their own vehicle
+If the rider is not over 16, has no insurance, OR has no car but they CAN get a rideshare, then they call up a rideshare service
+If the rider meets none of the above, they should call a friend for a ride!
+    This getToDestination should not return any value, but rather console log based on the conditions met.
+    getToDestination, part 2
+Create a function called canGetRideshare() which will RETURN a boolean
+It will determine if there are drivers nearby and the person has enough money for the ride
+Now, when you call getToDestination(), instead of passing in a canGetRideShare parameter
+Call canGetRideshare() in the else if after !isLegalDriver &&...
+Be sure to log to the user if they can or cannot get a rideshare */
+
+let age = 16;
+let isInsured = true;
+let hasCar = true;
+
+function getToDestination(age, isInsured, hasCar, canGetRideshare){
+    if (age >= 16 && isInsured && hasCar) {
+        console.log("Rider can use their own vehicle");
+    }else if(age < 16 && (!isInsured || !hasCar) && canGetRideshare){
+        console.log("Rider can call for a rideshare");
+    }else{
+        console.log("Rider should call a friend for a ride");
+    }
+}
+
+let driverNearby = true;
+let hasMoneyForRide = true;
+
+function canGetRideshare(driverNearby, hasMoneyForRide){
+    if(driverNearby && hasMoneyForRide){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+getToDestination(age, isInsured, hasCar, canGetRideshare(driverNearby, hasMoneyForRide));
+
+
