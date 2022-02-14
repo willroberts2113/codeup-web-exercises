f74838c8583b1457a9a2d933de3f259501479648
Will Roberts <willroberts2113@gmail.com>
2022-02-14 10:22:32 -0500

    update

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,29 +1,34 @@
 "use strict";
 
 
-let addNumber = promptForOddNumberInRange(1, 50);
-let oddNumber = prompt("Please provide an odd number:  ");
+// let addNumber = promptForOddNumberInRange(1, 50);
+// let oddNumber = prompt("Please provide an odd number:  ");
+//
+// printOddNumbersInRangeWithSkip(1, 50, oddNumber);
+//
+// function printOddNumbersInRangeWithSkip(low, high, skipNumber) {
+//     for(let i = 0; i <= high; i = i + 2) {
+//         if (i === skipNumber && i % 2 === 0) {
+//             if(i === skipNumber){
+//
+//             }
+//         }
+//     }
+// }
+//
+// function promptForOddNumberInRange(low, high) {
+//     let result;
+//     while(true){
+//         result = prompt("Enter an odd number from " + low + " to " + high);
+//         if (result !== null && result % 2 === 1 && result >= low && result <= high) {
+//             break;
+//         }
+//     }
+//     return result;
+//
+// }
 
-printOddNumbersInRangeWithSkip(1, 50, oddNumber);
+// Write a function called findAverage that takes in an array of integers representing grades and returns the average.
+//     Example input: [95, 74, 86, 100]
+// Example output: 88.75 (edited)
 
-function printOddNumbersInRangeWithSkip(low, high, skipNumber) {
-    for(let i = 0; i <= high; i = i + 2) {
-        if (i === skipNumber && i % 2 === 0) {
-            if(i === skipNumber){
-
-            }
-        }
-    }
-}
-
-function promptForOddNumberInRange(low, high) {
-    let result;
-    while(true){
-        result = prompt("Enter an odd number from " + low + " to " + high);
-        if (result !== null && result % 2 === 1 && result >= low && result <= high) {
-            break;
-        }
-    }
-    return result;
-
-}
