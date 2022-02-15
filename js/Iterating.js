09d87c2a193117f3c848ae01d293e53d8357ffd6
Will Roberts <willroberts2113@gmail.com>
2022-02-15 14:23:35 -0500

    update

--- js/Iterating.js
+++ js/Iterating.js
@@ -7,11 +7,15 @@
      * 'names'.
      */
 
+    let names = ["Ned", "John", "Robert", "Bran"];
+
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+
+    console.log(names.length);
 
     /**
      * TODO:
@@ -19,16 +23,27 @@
      * accessing each element's index.
      */
 
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
 
+    for(let i = 0; i < names.length; i++){
+        console.log(names[i]);
+    }
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+
+    names.forEach(element => console.log(element));
 
     /**
      * TODO:
@@ -43,5 +58,7 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+
 
 })();
