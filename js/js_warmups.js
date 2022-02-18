2b093ab280a8f949591a72aab606c6ff1b00f222
Will Roberts <willroberts2113@gmail.com>
2022-02-18 10:12:49 -0500

    js warmups

---
+++ js/js_warmups.js
@@ -0,0 +1,25 @@
+"use strict"
+
+// function getNames(){
+//     let namesArr = [];
+//     let hasMoreNames = true;
+//     while(hasMoreNames){
+//         namesArr.push(prompt("Please enter your desired name"));
+//         hasMoreNames = confirm("Would you like to enter another name");
+//     }
+//     return namesArr;
+// }
+//
+// getNames();
+
+let value = "asdasdasd"
+
+function isTrue(value){
+    if(parseFloat(isNaN(value)))    {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+isTrue(value);
