402456bba88bf80b0946420c7ba7a7000c85e7d7
Will Roberts <willroberts2113@gmail.com>
2022-02-28 10:33:37 -0500

    warm up in sandbox

--- js/sandbox.js
+++ js/sandbox.js
@@ -7,32 +7,53 @@
 // Hard code some values in that object, then log it and its properties to the console
 
 // Defines a contact info object
-function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress){
-    this.phoneNumber = phoneNumber;
-    this.streetAddress = streetAddress;
-    this.city = city;
-    this.state = state;
-    this.postalCode = postalCode;
-    this.emailAddress = emailAddress;
-}
-// Defines a person object
-function Person(names, occupation, dob, contactInfo) {
-    this.names = names;
-    this.occupation = occupation;
-    this.dob = dob;
-    this.contactInfo = contactInfo;
-}
-// makes a new person object instance (by invoking the constructor)
-let person2 = new Person(
-    ["Casey", "J", "Edwards"],
-    "Cat Herder / Firefighter",
-    "11.30.1989",
-    new ContactInfo(
-        '123.456.7890',
-        '123 Anywhere Ln',
-        'Beverly Hills',
-        'CA',
-        '90210',
-        'nope@no-no-no.no'));
+// function ContactInfo(phoneNumber, streetAddress, city, state, postalCode, emailAddress){
+//     this.phoneNumber = phoneNumber;
+//     this.streetAddress = streetAddress;
+//     this.city = city;
+//     this.state = state;
+//     this.postalCode = postalCode;
+//     this.emailAddress = emailAddress;
+// }
+// // Defines a person object
+// function Person(names, occupation, dob, contactInfo) {
+//     this.names = names;
+//     this.occupation = occupation;
+//     this.dob = dob;
+//     this.contactInfo = contactInfo;
+// }
+// // makes a new person object instance (by invoking the constructor)
+// let person2 = new Person(
+//     ["Casey", "J", "Edwards"],
+//     "Cat Herder / Firefighter",
+//     "11.30.1989",
+//     new ContactInfo(
+//         '123.456.7890',
+//         '123 Anywhere Ln',
+//         'Beverly Hills',
+//         'CA',
+//         '90210',
+//         'nope@no-no-no.no'));
+//
+// console.log(person2.contactInfo.emailAddress);
 
-console.log(person2.contactInfo.emailAddress);
+var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+
+console.log(removeAll(bugs, "ant"));
+
+// should print
+// [
+//     // 'mosquito',
+//     // 'scorpion',
+//     // 'mosquito',
+//     // 'typo',
+//     // 'reference error',
+//     // 'type error'
+// ]
+
+function removeAll(bugs) {
+    let newbugs = bugs.slice(1, 3, 4, 5);
+    console.log(newbugs);
+}
+
+
